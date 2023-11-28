f0baf77556d626090e8c74857fd4ea0c32d27dca
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-11-29 01:28:54 +0530

    Update start.py

--- Zuli/modules/start.py
+++ Zuli/modules/start.py
@@ -4,7 +4,7 @@ from Zuli import Zuli
 from config import BOT_USERNAME
 
 start_txt = """**
-ɪ ᴀᴍ ᴢᴜʟɪ
+ɪ ᴀᴍ ɴᴏᴛᴛʏʏ
 
 ɪ ᴀᴍ ʙᴜɪʟᴛ ʙʏ ᴄᴏᴍʙɪɴɪɴɢ ᴠᴀʀɪᴏᴜꜱ ᴀɪ ᴄᴀᴘᴀʙɪʟɪᴛɪᴇꜱ. ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴀɴꜱᴡᴇʀꜱ ᴛᴏ ᴀʟʟ ʏᴏᴜʀ Qᴜᴇꜱᴛɪᴏɴꜱ, ᴀɴᴅ ʏᴏᴜ ᴄᴀɴ ᴀꜱᴋ ᴍᴇ ᴀɴʏ Qᴜᴇꜱᴛɪᴏɴꜱ ʏᴏᴜ ʜᴀᴠᴇ. ᴀᴅᴅɪᴛɪᴏɴᴀʟʟʏ, ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ɪᴍᴀɢᴇꜱ ᴀɴᴅ ᴇᴠᴇɴ ᴇᴅɪᴛ ᴄᴏɴᴛᴇɴᴛ. ɪ ʜᴀᴠᴇ ᴍᴀɴʏ ꜰᴇᴀᴛᴜʀᴇꜱ ᴀᴛ ᴍʏ ᴅɪꜱᴘᴏꜱᴀʟ
 **"""
@@ -16,18 +16,20 @@ start_txt = """**
 async def start(_, msg):
     buttons = [
         [ 
-          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+          InlineKeyboardButton("𝐒ᴜᴍᴍᴏɴ 𝐌ᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/DevsOops"),
-          InlineKeyboardButton("ᴅᴇᴠ", url="https://t.me/iam_daxx"),
-          InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help_")
+          InlineKeyboardButton("𝐔ᴘᴅᴀᴛᴇs", url="https://t.me/AloneXBots"),
+          InlineKeyboardButton("𝐃ᴇᴠ", url="https://t.me/ALONE_WAS_BOT"),
+        ],
+        [
+          InlineKeyboardButton("𝐂ᴏᴍᴍᴀɴᴅs", callback_data="help_")
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://telegra.ph/file/f2a5f823b4c0793452504.jpg",
+        photo="https://telegra.ph/file/233a6f44e3f009c1e6724.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
